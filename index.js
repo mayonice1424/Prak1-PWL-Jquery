333d6fa7131bbd7aed8186664f94da296285b02b
mayonice1424 <friyandrailham14@gmail.com>
2022-05-05 15:14:11 +0700

    menambahkan container

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // menyiapkan database dengan format json
   let stokProduk = [
     {
       namaBarang: "Jet Tempur",
@@ -29,17 +30,17 @@ $(document).ready(function () {
     "Rudal Hipersonik",
   ];
   
-  // hiding btn tambah
-  var num = 1;
+  //menyembunyikan tombol tambah produk
+  var n = 1;
   $("#btn-tambah").hide();
-  $("#produk-" + num).change(function () {
+  $("#produk-" + n).change(function () {
     $("#btn-tambah").show();
   });
   
-  //pesan produk
+  //menampilkan hasil pesanan produk berdasarkan index
   let pesanan = "";
   $("#btn-pesan").click(function () {
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= n; i++) {
       let jumlah_idx = stokProduk.findIndex(
         (data) => data.namaBarang === $("#produk-" + i).val()
       );
@@ -62,32 +63,32 @@ $(document).ready(function () {
     }
   });
 
-  //tambah produk
+  //menambahkan produk
   $("#btn-tambah").click(function () {
     let jumlah_idx = stokProduk.findIndex(
-      (data) => data.namaBarang === $("#produk-" + num).val(),
+      (data) => data.namaBarang === $("#produk-" + n).val(),
     );
-    num += 1;
-    let pesanan = `<div class="clearfix cont2"><div class="left" id="id-${num}"><label for="produk-${num}">Produk</label><br><select id="produk-${num}" required><option value="" hidden selected>Pilih produk</option>`;
-    
+    n += 1;
+    let pesanan = `<div class="cont-2"><div class="left" id="id-${n}"><label for="produk-${n}">Produk</label><br><select id="produk-${n}" required><option value="" hidden selected>Pilih produk</option>`;
+    //mapping produk ke select
     products.map((data) => {
       pesanan += `<option value="${data}">${data}</option>`;
     }
   );
-
-    pesanan += `</select><br></div><div class="left cont3"><label for="jumlah-${num}" style="padding-left: 5px;">Jumlah</label><br><input type="number" id="jumlah-${num}" class="jumlah" required><br></div>`;
-    pesanan += `<div id='button-delete' class="left pd-top"><button id="btn-del-${num}" class="btn-3"><div class = box style="padding-top:5px;color:red;border-color:red;">x</div></button></div>`;
+    //membuat input jumlah dan tombol hapus
+    pesanan += `</select><br></div><div class="left-cont-3"><label for="jumlah-${n}" style="padding-left: 5px;">Jumlah</label><br><input type="nber" id="jumlah-${n}" class="jumlah" required><br></div>`;
+    pesanan += `<div id='button-delete' class="left pd-top"><button id="btn-del-${n}" class="btn-3"><div class = box style="padding-top:5px;color:red;border-color:red;">x</div></button></div>`;
     $(this).before(pesanan);
 
-    $("#btn-del-" + num).click(function () {
-      $("#id-" + num).nextAll();
+    $("#btn-del-" + n).click(function () {
+      $("#id-" + n).nextAll();
       $(this).parent().parent().remove();
-      $("#id-" + num).closest("div");
+      $("#id-" + n).closest("div");
       $(this).parent().parent().remove();
     });
   });
 });
-
+// langsung menampilkan nama ketika input nama telah diisi
 function nama (){
   var nama = $("#nama").html(`${$("#nama-pemesan").val()}`);
   $("#nama").show() = nama;
